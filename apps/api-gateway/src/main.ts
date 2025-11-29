f7a9ab6f3eec7f6271156f912c77f380e046ab02
Edd michira <eddmichira@gmail.com>
2025-11-29 11:53:36 +0300

    epharma initialized

---
+++ apps/api-gateway/src/main.ts
@@ -0,0 +1,56 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import express from 'express';
+import cors from 'cors';
+import proxy from 'express-http-proxy';
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import swaggerUi from 'swagger-ui-express';
+import axios from 'axios';
+import cookieParser from 'cookie-parser';
+
+// import * as path from 'path';
+
+const app = express();
+
+app.use (
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  })
+);
+app.use(morgan("dev"));
+app.use(express.json({limit: "100mb"}));
+app.use(express.urlencoded({limit: "100mb", extended: true}));
+app.use(cookieParser());
+app.set("trust proxy", 1)
+
+// apply rate limiting
+const limiter = rateLimit({
+  windowMs: 15*60*1000,
+  max: (req:any) => (req.user? 1000 : 100),
+  message: { error: "Too many request, please try again later!"},
+  standardHeaders: true,
+  legacyHeaders: true,
+  keyGenerator: (req: any)=>req.ip,
+});
+
+app.use (limiter);
+
+// app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/gateway-health', (req, res) => {
+  res.send({ message: 'Welcome to api-gateway!' });
+});
+
+app.use("/", proxy("http://localhost:6001"));
+
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
