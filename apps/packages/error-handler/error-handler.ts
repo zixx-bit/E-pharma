34689e416f0c24f409c8b5327838768572cf7ded
Edd michira <eddmichira@gmail.com>
2025-12-09 14:10:26 +0300

    Update error-handler.ts

--- apps/packages/error-handler/error-handler.ts
+++ apps/packages/error-handler/error-handler.ts
@@ -1,4 +1,20 @@
 
+import { AppError } from "./index";
+import { Response, Request } from "express";
+
 export const errorMiddleware = (err: Error, req: Request, res: Response) =>{
-    if (err instanceof AppError)    
+    if (err instanceof AppError) {
+        console.log(`Error: ${req.method} ${req.url} - ${err.message}`);
+
+        return res.status(err.statusCode).json({
+            status: "error",
+            message:err.message,
+            ...(err.details && { details: err.details }),
+        }) 
+    } 
+    
+    console.log("Unhandled error:", err);
+    return res.status(500).json({
+        error: "Something went wrong, please try again!",
+    });
 }
