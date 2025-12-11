41f7f02f4352ecdd699379e99da6463e75b1cc2c
Edd michira <eddmichira@gmail.com>
2025-12-11 22:58:10 +0300

    Create auth.helper file

---
+++ apps/auth-service/src/utils/auth.helper.ts
@@ -0,0 +1,21 @@
+import crypto from "crypto";
+
+import { ValidationError } from "../../../packages/error-handler/index.js";
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+
+export const validateRegistrationData = (data: any, userType: "user" | "seller") => {
+
+    const { name, email, password, phone_number, country } = data;
+
+    if (!name || !email|| !password ||(userType ==="seller" && (!phone_number || !country))) {
+        throw new ValidationError(`Missing required fields!`)        
+    }
+
+    if (!emailRegex.test(email)) {
+        throw new ValidationError("Invalid email format!")
+        
+    }
+
+}
