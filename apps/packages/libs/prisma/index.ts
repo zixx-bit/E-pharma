2571495287708e93fbaf0b618de5219f3e218354
Edd michira <eddmichira@gmail.com>
2025-12-11 23:07:56 +0300

    Create prisma client file

---
+++ apps/packages/libs/prisma/index.ts
@@ -0,0 +1,13 @@
+import {PrismaClient} from "@prisma/client";
+
+declare global {
+    namespace globalThis{
+        var prismadb: PrismaClient;
+    }
+};
+
+const prisma = new PrismaClient()
+
+if (process.env.NODE_ENV === "production") global.prismadb = prisma;
+
+export default prisma;
