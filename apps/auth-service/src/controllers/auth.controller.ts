84f4a6c03247022203b2022ae3ed643dd4742944
Edd michira <eddmichira@gmail.com>
2025-12-11 22:58:35 +0300

    Create auth.controller file

---
+++ apps/auth-service/src/controllers/auth.controller.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response } from "express"
+// Register new user
+
+export const userRegistration = async(req: Request, res: Response, next: NextFunction) =>{
+    
+
+}
