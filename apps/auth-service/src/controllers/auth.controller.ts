27655d49c8e2ffb1cda6ce2762036371c055173c
Edd michira <eddmichira@gmail.com>
2025-12-16 21:56:54 +0300

    Update auth.controller.ts

--- apps/auth-service/src/controllers/auth.controller.ts
+++ apps/auth-service/src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express"
 import { validateRegistrationData } from "../utils/auth.helper.js"
 import prisma from "../../../../packages/libs/prisma/index.js";
+import { ValidationError } from "../../../../packages/error-handler/index.js";
 
 // Register new user
 export const userRegistration = async(req: Request, res: Response, next: NextFunction) =>{
@@ -8,5 +9,13 @@ export const userRegistration = async(req: Request, res: Response, next: NextFun
     validateRegistrationData(req.body, "user");
     const {name, email} = req.body
 
-    const isUserExisting = await prisma.users
+    const isUserExisting = await prismadb.users.findUnique({where: email});
+
+    if (isUserExisting) {
+        return next(new ValidationError("User already exists with this email"));
+    };
+
+    await checkOtpRestrictions(email, next);
+
+
 }
