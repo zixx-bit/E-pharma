387ca1f9c04144950c7b6d7450a507cb3c897069
Edd michira <eddmichira@gmail.com>
2025-12-17 23:57:59 +0300

    Update index.ts

--- apps/auth-service/src/utils/sendMail/index.ts
+++ apps/auth-service/src/utils/sendMail/index.ts
@@ -13,7 +13,36 @@ const transporter = nodemailer.createTransport({
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
+});
 
+// Render an EJS email template
+const renderEmailTemplate = async(templateName: string, data:Record<string,any>): Promise <string> =>{
+    const templatePath = path.join(
+        process.cwd(),
+        "auth-service",
+        "src",
+        "utils",
+        "email-templates",
+        `${templateName}.ejs`
+    );
+    return ejs.renderFile(templatePath, data)
+};
 
-})
+// send an email using nodemailer
+export const sendEmail = async(to:string, subject: string, templateName:string, data: Record<string, any>)=>{
+    try {
+        const html = await renderEmailTemplate(templateName, data);
+        await transporter.sendMail({
+            from: `<${process.env.SMTP_USER}`,
+            to,
+            subject,
+            html,
+        });
+        return true;
+        
+    } catch (error) {
+        console.log("Error sending email", error);
+        return false;
+    }
+}
 
