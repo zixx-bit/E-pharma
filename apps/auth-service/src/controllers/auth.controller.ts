d2bdd14934d754c353df29beb1cc4d55b9c0e460
Edd michira <eddmichira@gmail.com>
2026-01-03 14:58:52 +0300

    Update auth.controller.ts

--- apps/auth-service/src/controllers/auth.controller.ts
+++ apps/auth-service/src/controllers/auth.controller.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response } from "express"
-import { checkOtpRestrictions, validateRegistrationData } from "../utils/auth.helper.js"
+import { checkOtpRestrictions, sendOtp, trackOtpRequests, validateRegistrationData } from "../utils/auth.helper.js"
 import prisma from "../../../../packages/libs/prisma/index.js";
 import { ValidationError } from "../../../../packages/error-handler/index.js";
 
 // Register new user
 export const userRegistration = async(req: Request, res: Response, next: NextFunction) =>{
-    
+  try {
+      
     validateRegistrationData(req.body, "user");
     const {name, email} = req.body
 
@@ -16,8 +17,15 @@ export const userRegistration = async(req: Request, res: Response, next: NextFun
     };
 
     await checkOtpRestrictions(email, next);
+    await trackOtpRequests(email, next);
+    await sendOtp(email, name, "user-actiavtion-mail");
 
-    await trackOtpRequests(email, next)
+    res.status(200).json({
+        message: "OTP sent to email. Please verify your account."
+    })
 
 
+  } catch (error) {
+    return next(error);
+  }
 }
