fbdcd96998a28564b8c423255d45a624f779a498
Edd michira <eddmichira@gmail.com>
2026-01-07 23:26:32 +0300

    Create swagger.js

---
+++ apps/auth-service/src/swagger.js
@@ -0,0 +1,16 @@
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+    info: {
+        title: "Auth Service API",
+        description: "Automatically generated Swagger docs",
+        version: "1.0.0",
+    },
+    host: "localhost:6001",
+    schemes: ["http"],
+};
+
+const outputFile = "./swagger-output.json";
+const endpointsFiles = ["./routes/auth.router.ts"];
+
+swaggerAutogen()(outputFile, endpointsFiles,doc);
