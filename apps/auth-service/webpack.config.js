32d038de154617dd854b041ae931bafe93dc375c
Edd michira <eddmichira@gmail.com>
2026-01-19 12:18:18 +0300

    Update webpack.config.js

--- apps/auth-service/webpack.config.js
+++ apps/auth-service/webpack.config.js
@@ -1,21 +1,29 @@
 const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
-const { join } = require('path');
+const { join, resolve, dirname } = require('path');
 
 module.exports = {
   output: {
     path: join(__dirname, 'dist'),
-    clean: true,
-    ...(process.env.NODE_ENV !== 'production' && {
-      devtoolModuleFilenameTemplate: '[absolute-resource-path]',
-    }),
+    // clean: true,
+    // ...(process.env.NODE_ENV !== 'production' && {
+    //   devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    // }),
   },
+  resolve:{
+    alias: {
+      "@packages": resolve(__dirname, '../../packages')
+
+    },
+    extensions: [".ts", ".js"],
+  },
+
   plugins: [
     new NxAppWebpackPlugin({
       target: 'node',
       compiler: 'tsc',
       main: './src/main.ts',
       tsConfig: './tsconfig.app.json',
-      assets: ["./src/assets"],
+      // assets: ["./auth-service/src/assets"],
       optimization: false,
       outputHashing: 'none',
       generatePackageJson: true,
