1c6a148553057bba31bfb8a80f2e347c775cb86e
Edd michira <eddmichira@gmail.com>
2026-01-29 19:42:10 +0300

    Update auth.router.ts

--- apps/auth-service/src/routes/auth.router.ts
+++ apps/auth-service/src/routes/auth.router.ts
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
 
-import { userRegistration, verifyUser } from "../controllers/auth.controller.js";
+import { loginUser, userRegistration, verifyUser } from "../controllers/auth.controller.js";
 
 const router: Router = express.Router();
 router.post("/user-registration", userRegistration);
 router.post("/verify-user", verifyUser);
+router.post("/login-user", loginUser);
 
 export default router;
