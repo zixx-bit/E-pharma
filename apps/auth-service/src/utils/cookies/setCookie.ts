c81bc1d0607be9e4ee21c0db2f4b602e4c6f7e98
Edd michira <eddmichira@gmail.com>
2026-01-29 15:51:09 +0300

    Create setCookie.ts

---
+++ apps/auth-service/src/utils/cookies/setCookie.ts
@@ -0,0 +1,11 @@
+import { Response } from "express";
+
+export const setCookie =  (res: Response, name: string, value: string)=>
+    {
+        res.cookie(name, value, {
+            httpOnly: true, 
+            secure: true,
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+    };
