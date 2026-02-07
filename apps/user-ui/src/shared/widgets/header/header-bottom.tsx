d576b77d759e5df92f8295b30abd1cf19b80d251
Edd michira <eddmichira@gmail.com>
2026-02-07 17:43:36 +0300

    Update header-bottom.tsx

--- apps/user-ui/src/shared/widgets/header/header-bottom.tsx
+++ apps/user-ui/src/shared/widgets/header/header-bottom.tsx
@@ -20,24 +20,39 @@ const HeaderBottom = () => {
   }, []);
   return (
     <div
-      className={`w-full transition-all duration-300 ${
-        isSticky ? "fixed top-0 left-0 z-[100] bg-white shadow-lg" : "relative"
-      }`}
+      className={`w-full transition-all duration-300 ${isSticky ? "fixed top-0 left-0 z-[100] bg-white shadow-lg" : "relative"
+        }`}
     >
       <div
-        className={`w-[80%] relative m-auto flex items-center justify-between ${
-          isSticky ? "pt-3" : "py-0"
-        }`}
+        className={`w-[80%] relative m-auto flex items-center justify-between ${isSticky ? "pt-3" : "py-0"
+          }`}
       >
         {/* All dropdowns */}
         <div className={`w-[260px] ${isSticky && 'mb-2'} cursor-pointer flex items-center justify-between px-5 h-[50px] bg-[#2489ff] `
-            }
-            onClick={()=> setShow(!show)}>
-            <div className="flex items-center gap-2">
-                <AlignLeft color="white"/>
-                <span className="text-white font-medium">All Categories</span>
+        }
+          onClick={() => setShow(!show)}>
+          <div className="flex items-center gap-2">
+            <AlignLeft color="white" />
+            <span className="text-white font-medium">All Categories</span>
+          </div>
+          <ChevronDown color="white" />
+        </div>
+        <div>
+          {/* Dropdown menu */}
+          {show && (
+            <div className={`absolute left-0 ${isSticky ? "top-[70px]" : "top-[50px]"} 
+            w-[260px] h-[400px] bg-[#f5f5f5] shadow-md rounded-lg p-4`}
+            
+            
+            >
             </div>
-            <ChevronDown color="white"/>
+
+          )}
+
+          {/* Navigation links */}
+          <div className="flex items-center gap-2">
+
+          </div>
         </div>
       </div>
       HeaderBottom
