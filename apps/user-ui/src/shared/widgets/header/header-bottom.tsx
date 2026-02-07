d3f2cfc4f30c624c19a3f3ee6d8db443b56516d8
Edd michira <eddmichira@gmail.com>
2026-02-07 12:15:18 +0300

    Create header-bottom.tsx

---
+++ apps/user-ui/src/shared/widgets/header/header-bottom.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { AlignLeft, ChevronDown } from "lucide-react";
+import React, { useEffect, useState } from "react";
+
+const HeaderBottom = () => {
+  const [show, setShow] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
+
+  // ?track scroll position
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.screenY > 100) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <div
+      className={`w-full transition-all duration-300 ${
+        isSticky ? "fixed top-0 left-0 z-[100] bg-white shadow-lg" : "relative"
+      }`}
+    >
+      <div
+        className={`w-[80%] relative m-auto flex items-center justify-between ${
+          isSticky ? "pt-3" : "py-0"
+        }`}
+      >
+        {/* All dropdowns */}
+        <div className={`w-[260px] ${isSticky && 'mb-2'} cursor-pointer flex items-center justify-between px-5 h-[50px] bg-[#2489ff] `
+            }
+            onClick={()=> setShow(!show)}>
+            <div className="flex items-center gap-2">
+                <AlignLeft color="white"/>
+                <span className="text-white font-medium">All Categories</span>
+            </div>
+            <ChevronDown color="white"/>
+        </div>
+      </div>
+      HeaderBottom
+    </div>
+  );
+};
+
+export default HeaderBottom;
