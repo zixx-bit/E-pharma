29f9860d5b519a811b09611080654d79362bdd03
Edd michira <eddmichira@gmail.com>
2026-02-20 11:46:32 +0300

    Update contansts.tsx

---
+++ apps/user-ui/src/configs/contansts.tsx
@@ -0,0 +1,30 @@
+export const navItems: NavItemsTypes[] = [
+    {
+        title: "Home",
+        href: "/",
+    },
+    {
+        title: "Products",
+        href: "/products",
+    },
+    // {
+    //     title: "Shops",
+    //     href: "/shops",
+    // },
+    {
+        title: "Offers",
+        href: "/offers",
+    },
+    // {
+    //     title: "Become a seller",
+    //     href: "/become-seller",
+    // },
+    {
+        title: "About",
+        href: "/about",
+    },
+    {
+        title: "Contact",
+        href: "/contact",
+    },
+];
