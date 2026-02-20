65b3d112ec4e71377ea5576664465f03995cfa7f
Edd michira <eddmichira@gmail.com>
2026-02-20 12:27:37 +0300

    Create page.tsx

---
+++ apps/user-ui/src/app/(routes)/signup/page.tsx
@@ -0,0 +1,128 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { useForm } from "react-hook-form";
+import React, { useState } from 'react'
+import Link from 'next/link';
+import GoogleButton from 'apps/user-ui/src/shared/google-button';
+import { Eye, EyeOff } from 'lucide-react';
+
+type FormData = {
+    email: string;
+    password: string;
+}
+const Login = () => {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [serverError, setServerError] = useState<string | null>(null);
+    const [rememberMe, setRememberMe] = useState(false);
+    const router = useRouter
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+
+    const onSubmit = (data: FormData) => {
+        console.log(data);
+    }
+    return (
+        <div className='w-full py-10 min-h-[85vh] bg-[#f1f1f1] '>
+            <h1 className='text-4xl font-Poppins font-semibold text-black text-center'>
+                Login
+            </h1>
+            <p className='text-center text-lg font-medium py-3 text-[#00000099]'>
+                Home.Login
+            </p>
+            <div className='w-full flex justify-center'>
+                <div className='md:w-[480px] p-8 bg-white shadow rounded-lg'>
+                    <h3 className='text-3xl font-semibold text-center mb-2'>
+                        Login to AfyaNova Phamarcy
+                    </h3>
+                    <p className='text-center text-gray-500 mb-4'>
+                        Don't have an account?  {" "}
+                        <Link href={"/signup"} className='text-blue-500'>
+                            Sign up
+                        </Link>
+                    </p>
+                    <GoogleButton />
+                    <div className='flex items-center my-5 text-gray-400 text-sm'>
+                        <div className='flex-1 border-t border-gray-300' />
+                        <span className='px-3'> or Sign in with Email</span>
+                        <div className='flex-1 border-t border-gray-300'></div>
+                    </div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <label className='block text-gray-700 mb-1'>Email</label>
+                        <input
+                            type='email'
+                            placeholder='support@afyanova.co.ke'
+                            className='w-full p-2 border border-gray-300 outline-0 !rounded mb-1'
+                            {...register("email", {
+                                required: "Email is requires",
+                                pattern: {
+                                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                                    message: "Invalid email address",
+                                }
+
+                            })}>
+                        </input>
+                        {errors.email && (
+                            <p className='text-red-500 text-sm'>
+                                {String(errors.email.message)}
+                            </p>
+                        )}
+
+                        <label className='block text-gray-700 mb-1'>Password</label>
+                        <div className='relative'>
+                        <input
+                            type={passwordVisible ? "text" : "password"}
+                            placeholder='Min. 6 characters'
+                            className='w-full p-2 border border-gray-300 outline-0 !rounded mb-1'
+                            {...register("password", {
+                                required: "Password is requires",
+                                minLength: {
+                                    value: 6,
+                                    message: "Password must be at least 6 characters",
+                                },
+
+                            })}>
+                        </input>
+                       <button type='button'
+                       onClick={()=> setPasswordVisible(!passwordVisible)}
+                       className='absolute inset-y-0 right-3 flex items-center text-gray-400'>
+                       {passwordVisible ? <Eye/>  : <EyeOff/>}
+                       </button>
+                       {errors.password && (
+                            <p className='text-red-500 text-sm'>
+                                {String(errors.password.message)}
+                            </p>
+                        )}
+                        </div>
+                        <div className='flex justify-between items-center my-4'>
+                            <label className='flex items-center text-gray-600'>
+                                <input
+                                type='checkbox'
+                                className='mr-2'
+                                checked={rememberMe}
+                                onChange={() => setRememberMe(!rememberMe)}>
+                                </input>
+                                Remember me
+                            </label>
+                            <Link href={"/forgot-password"} className='text-blue-500 text-sm'>
+                            Forgot password
+                            </Link>
+                        </div>
+                        <button type='submit'
+                        className='w-full text-lg cursor-pointer bg-black text-white py-2 rounded-lg'>
+                          Login  
+                        </button>
+                        {serverError && (
+                            <p className='text-red-500 text-sm mt-2'>
+                                {serverError}
+                            </p>
+                        )}
+
+
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Login;
