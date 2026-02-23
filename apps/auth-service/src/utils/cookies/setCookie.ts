7bdbcc15ec57feddd447285cc88ecabed41c0994
Edd michira <eddmichira@gmail.com>
2026-02-23 22:55:54 +0300

    Update setCookie.ts

--- apps/auth-service/src/utils/cookies/setCookie.ts
+++ apps/auth-service/src/utils/cookies/setCookie.ts
@@ -1,6 +1,6 @@
 import { Response } from "express";
 
-export const setCookie =  (res: Response, name: string, value: string)=>
+export const setCookie =  (res: Response, name: string, value: string) =>
     {
         res.cookie(name, value, {
             httpOnly: true, 
