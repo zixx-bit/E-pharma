5afa962e2b8fc539836a8d5c281c8266ceb3818d
Edd michira <eddmichira@gmail.com>
2026-03-06 12:00:17 +0300

    Update providers.tsx

--- apps/user-ui/src/app/providers.tsx
+++ apps/user-ui/src/app/providers.tsx
@@ -2,11 +2,15 @@
 
 import React, { useState } from 'react'
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { Toaster } from 'react-hot-toast';
 
 const Providers = ({children}: {children:React.ReactNode}) => {
     const [queryClient] = useState(()=> new QueryClient());
   return (
-    <QueryClientProvider client = {queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <Toaster />
+    </QueryClientProvider>
   );
 }
 
