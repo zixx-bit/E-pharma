b207f752eca38df436fba3a75e165ac5d5a05495
Edd michira <eddmichira@gmail.com>
2026-03-06 10:24:20 +0300

    Update page.tsx

--- apps/user-ui/src/app/(routes)/signup/page.tsx
+++ apps/user-ui/src/app/(routes)/signup/page.tsx
@@ -5,6 +5,8 @@ import React, { useRef, useState } from 'react'
 import Link from 'next/link';
 import GoogleButton from 'apps/user-ui/src/shared/google-button';
 import { Eye, EyeOff } from 'lucide-react';
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
 
 type FormData = {
     name: string;
@@ -26,30 +28,59 @@ const Signup = () => {
     const router = useRouter
     const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
 
+    const startResendTimer = () => {
+        const timerInterval = setInterval(() => {
+            setTimer((prev) => {
+                if (prev <= 1) {
+                    clearInterval(timerInterval);
+                    setCanResend(true);
+                    return 0;
+                }
+                return prev-1;                
+            });
+        }, 1000);
+    };
+
+    const signupMutation = useMutation({
+        mutationFn: async (data: FormData) => {
+            const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URI}/api/user-registration`, data);
+            return response.data; 
+        },
+        onSuccess: (_, FormData)=>{
+            setUserData(FormData);
+            setShowOtp(true);
+            setCanResend(false);
+            setTimer(60);
+            startResendTimer();
+        },
+    });
+
+
+
     const onSubmit = (data: FormData) => {
-        console.log(data);
+        signupMutation.mutate(data);
     }
 
-    const handleOtpChange = (index:number, value:string) =>{
-        if(!/^[0-9]?$/.test(value)) return;
+    const handleOtpChange = (index: number, value: string) => {
+        if (!/^[0-9]?$/.test(value)) return;
 
         const newOtp = [...otp];
         newOtp[index] = value;
         setOtp(newOtp);
 
-        if (value && index < inputRefs.current.length - 1)  {
+        if (value && index < inputRefs.current.length - 1) {
             inputRefs.current[index + 1]?.focus();
         }
     };
 
-    const handleOtpKeyDown = (index: number, e:React.KeyboardEvent<HTMLInputElement>)=>{
+    const handleOtpKeyDown = (index: number, e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === "Backspace" && !otp[index] && index > 0) {
-            inputRefs.current[index-1]?.focus();
+            inputRefs.current[index - 1]?.focus();
         }
     }
 
-    const resendOtp = () =>{
-        
+    const resendOtp = () => {
+
     }
 
 
@@ -69,7 +100,7 @@ const Signup = () => {
                         Sign up to AfyaNova Phamarcy
                     </h3>
                     <p className='text-center text-gray-500 mb-4'>
-                    Already have an account ?  {" "}
+                        Already have an account ?  {" "}
                         <Link href={"/login"} className='text-blue-500'>
                             Log In
                         </Link>
@@ -80,15 +111,15 @@ const Signup = () => {
                         <span className='px-3'> or Sign in with Email</span>
                         <div className='flex-1 border-t border-gray-300'></div>
                     </div>
-                    {!showOtp ?     (<form onSubmit={handleSubmit(onSubmit)}>
-                    <label className='block text-gray-700 mb-1'>Name</label>
+                    {!showOtp ? (<form onSubmit={handleSubmit(onSubmit)}>
+                        <label className='block text-gray-700 mb-1'>Name</label>
                         <input
                             type='text'
                             placeholder='Enter your name'
                             className='w-full p-2 border border-gray-300 outline-0 !rounded mb-1'
                             {...register("name", {
                                 required: "Name is required",
-                              
+
                             })}>
                         </input>
                         {errors.name && (
@@ -119,34 +150,35 @@ const Signup = () => {
 
                         <label className='block text-gray-700 mb-1'>Password</label>
                         <div className='relative'>
-                        <input
-                            type={passwordVisible ? "text" : "password"}
-                            placeholder='Min. 6 characters'
-                            className='w-full p-2 border border-gray-300 outline-0 !rounded mb-1'
-                            {...register("password", {
-                                required: "Password is requires",
-                                minLength: {
-                                    value: 6,
-                                    message: "Password must be at least 6 characters",
-                                },
+                            <input
+                                type={passwordVisible ? "text" : "password"}
+                                placeholder='Min. 6 characters'
+                                className='w-full p-2 border border-gray-300 outline-0 !rounded mb-1'
+                                {...register("password", {
+                                    required: "Password is requires",
+                                    minLength: {
+                                        value: 6,
+                                        message: "Password must be at least 6 characters",
+                                    },
 
-                            })}>
-                        </input>
-                       <button type='button'
-                       onClick={()=> setPasswordVisible(!passwordVisible)}
-                       className='absolute inset-y-0 right-3 flex items-center text-gray-400'>
-                       {passwordVisible ? <Eye/>  : <EyeOff/>}
-                       </button>
-                       {errors.password && (
-                            <p className='text-red-500 text-sm'>
-                                {String(errors.password.message)}
-                            </p>
-                        )}
+                                })}>
+                            </input>
+                            <button type='button'
+                                onClick={() => setPasswordVisible(!passwordVisible)}
+                                className='absolute inset-y-0 right-3 flex items-center text-gray-400'>
+                                {passwordVisible ? <Eye /> : <EyeOff />}
+                            </button>
+                            {errors.password && (
+                                <p className='text-red-500 text-sm'>
+                                    {String(errors.password.message)}
+                                </p>
+                            )}
                         </div>
-                      
+
                         <button type='submit'
-                        className='w-full text-lg cursor-pointer mt-4 bg-black text-white py-2 rounded-lg'>
-                          Sign Up  
+                            disabled={signupMutation.isPending}
+                            className='w-full text-lg cursor-pointer mt-4 bg-black text-white py-2 rounded-lg'>
+                           {signupMutation.isPending ? "Signing up..." : "Sign up"}
                         </button>
                         {serverError && (
                             <p className='text-red-500 text-sm mt-2'>
@@ -163,18 +195,18 @@ const Signup = () => {
                             <div className='flex justify-center gap-6'>
                                 {otp?.map((digit, index) => (
                                     <input
-                                    key={index}
-                                    type='text'
-                                    ref={(el)=>{
-                                        if (el) inputRefs.current[index]=el; {
-                                            
-                                        }
-                                    }}
-                                    maxLength={1}
-                                    className='w-12 h-12 text-center border border-gray-300 outline-none !rounded'
-                                    value={digit}
-                                    onChange={(e) => handleOtpChange(index, e.target.value)}
-                                    onKeyDown={(e) => handleOtpKeyDown(index, e)}>                                    
+                                        key={index}
+                                        type='text'
+                                        ref={(el) => {
+                                            if (el) inputRefs.current[index] = el; {
+
+                                            }
+                                        }}
+                                        maxLength={1}
+                                        className='w-12 h-12 text-center border border-gray-300 outline-none !rounded'
+                                        value={digit}
+                                        onChange={(e) => handleOtpChange(index, e.target.value)}
+                                        onKeyDown={(e) => handleOtpKeyDown(index, e)}>
                                     </input>
                                 ))}
                             </div>
@@ -185,15 +217,15 @@ const Signup = () => {
                              mt-4'>
                                 {canResend ? (
                                     <button onClick={resendOtp}
-                                    className='text-blue-500 cursor-pointer'>
+                                        className='text-blue-500 cursor-pointer'>
                                         Resend OTP
                                     </button>
-                                ): (`Resend OTP in ${timer}s`)}
+                                ) : (`Resend OTP in ${timer}s`)}
 
                             </p>
                         </div>
                     )}
-                
+
                 </div>
 
             </div>
