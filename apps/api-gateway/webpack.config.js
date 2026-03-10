224d7b9f16088400d0c38c7e38611e620364fbb5
Edd michira <eddmichira@gmail.com>
2026-03-10 13:24:52 +0300

    Update webpack.config.js

--- apps/api-gateway/webpack.config.js
+++ apps/api-gateway/webpack.config.js
@@ -1,25 +1,25 @@
-const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
-const { join } = require('path');
+const { NxAppWebpackPlugin } = require("@nx/webpack/app-plugin");
+const { join } = require("path");
 
 module.exports = {
   output: {
-    path: join(__dirname, 'dist'),
+    path: join(__dirname, "dist"),
     clean: true,
-    ...(process.env.NODE_ENV !== 'production' && {
-      devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    ...(process.env.NODE_ENV !== "production" && {
+      devtoolModuleFilenameTemplate: "[absolute-resource-path]",
     }),
   },
   plugins: [
     new NxAppWebpackPlugin({
-      target: 'node',
-      compiler: 'tsc',
-      main: './src/main.ts',
-      tsConfig: './tsconfig.app.json',
+      target: "node",
+      compiler: "tsc",
+      main: "./src/main.ts",
+      tsConfig: "./tsconfig.app.json",
       assets: ["./src/assets"],
       optimization: false,
-      outputHashing: 'none',
+      outputHashing: "none",
       generatePackageJson: false,
       sourceMaps: true,
-    })
+    }),
   ],
 };
