0b298e7a9b42915d315bb876eb0ca3a709049b42
Edd michira <eddmichira@gmail.com>
2026-03-10 13:24:37 +0300

    Update main.ts

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,49 +1,46 @@
-
-
-import express from 'express';
-import cors from 'cors';
-import proxy from 'express-http-proxy';
-import morgan from 'morgan';
-import rateLimit, {ipKeyGenerator} from 'express-rate-limit';
-import swaggerUi from 'swagger-ui-express';
-import axios from 'axios';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import proxy from "express-http-proxy";
+import morgan from "morgan";
+import rateLimit, { ipKeyGenerator } from "express-rate-limit";
+import swaggerUi from "swagger-ui-express";
+import axios from "axios";
+import cookieParser from "cookie-parser";
 
 // import * as path from 'path';
 
 const app = express();
 
-app.use (
+app.use(
   cors({
     origin: ["http://localhost:3000"],
     allowedHeaders: ["Authorization", "Content-Type"],
     credentials: true,
-  })
+  }),
 );
 app.use(morgan("dev"));
-app.use(express.json({limit: "100mb"}));
-app.use(express.urlencoded({limit: "100mb", extended: true}));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(cookieParser());
-app.set("trust proxy", 1)
+app.set("trust proxy", 1);
 
 // apply rate limiting
 const limiter = rateLimit({
-  windowMs: 15*60*1000,
-  max: (req:any) => (req.user? 1000 : 100),
-  message: { error: "Too many request, please try again later!"},
+  windowMs: 15 * 60 * 1000,
+  max: (req: any) => (req.user ? 1000 : 100),
+  message: { error: "Too many request, please try again later!" },
   standardHeaders: true,
   legacyHeaders: true,
   // keyGenerator: (req: any)=>req.ip,
   keyGenerator: (req: any) => ipKeyGenerator(req),
-
 });
 
-app.use (limiter);
+app.use(limiter);
 
 // app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/gateway-health', (req, res) => {
-  res.send({ message: 'Welcome to api-gateway!' });
+app.get("/gateway-health", (req, res) => {
+  res.send({ message: "Welcome to api-gateway!" });
 });
 
 app.use("/", proxy("http://localhost:6001"));
@@ -52,4 +49,4 @@ const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
-server.on('error', console.error);
+server.on("error", console.error);
